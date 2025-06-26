9dfe0455f1de90c1fab6d5bc122f0533a4c61016
KeshavMakkad <keshavmakkad10@gmail.com>
2025-06-26 14:16:50 +0530

    add posthog testing

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 import App from './App.jsx'
+import { PostHogProvider } from 'posthog-js/react'
+
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  defaults: '2025-05-24',
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY} options={options}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </PostHogProvider>
   </StrictMode>,
 )
